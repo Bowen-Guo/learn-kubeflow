586fb9af0fc69e6dc5bf8ed9cb3a581fdb8ce82f
Bowen Guo <bowguo@microsoft.com>
2019-05-28 18:19:13 +0800

    Add ps scipt to test pipeline

--- mnist/common_docker/build_image.ps1
+++ mnist/common_docker/build_image.ps1
@@ -1,7 +1,15 @@
 $DOCKER_NAME = "mnist-tensoorflow"
 $REPOSITORY_NAME = "guobowen1990/mnist-pipeline"
 $TAG = "latest"
+$RUN_TEST = 1
 
+if ($RUN_TEST) {
+    cd ..\test
+    .\test_pipeline.ps1
+    cd ..\common_docker
+}
+
+mkdir ./src
 cp ../src/* ./src/
 docker pull zzn2/tensorflow-with-conda
 docker build -t $DOCKER_NAME .
---
+++ mnist/test/test_pipeline.ps1
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+$TEST_DIR="./temp_folder"
+
+mkdir $TEST_DIR
+
+# Load data
+python ../src/load_data.py --output-data-folder-path=$TEST_DIR
+
+# Train model
+python ../src/train.py --input-data-folder-path=$TEST_DIR --output-model-folder-path=$TEST_DIR
+
+# Score model
+python ../src/score.py --input-learner-folder-path=$TEST_DIR --input-data-folder-path=$TEST_DIR --output-data-folder-path=$TEST_DIR
+
+# Evaluate model
+python ../src/evaluate.py --input-prediction-folder-path=$TEST_DIR --input-true-folder-path=$TEST_DIR --output-data-folder-path=$TEST_DIR
+
+Remove-Item -Force -Recurse $TEST_DIR
