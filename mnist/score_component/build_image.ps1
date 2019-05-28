5f900f9313d7daee3adb8e3416f462dbeae87f58
Bowen Guo <bowguo@microsoft.com>
2019-05-28 13:25:57 +0800

    Pull base image

--- mnist/score_component/build_image.ps1
+++ mnist/score_component/build_image.ps1
@@ -3,6 +3,7 @@ $REPOSITORY_NAME = "guobowen1990/mnist-score"
 $TAG = "latest"
 
 cp ../src/* ./src/
+docker pull zzn2/tensorflow-with-conda
 docker build -t $DOCKER_NAME .
 Remove-Item -Force -Recurse ./src
 docker tag $DOCKER_NAME ${REPOSITORY_NAME}:${TAG}
