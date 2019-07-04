6a396a67dc1f45a06f8bd68a11fe3838382d5166
Bowen Guo <bowguo@microsoft.com>
2019-07-04 11:08:14 +0800

    Update ner-bert to 0.0.2

--- ner-bert/setup.py
+++ ner-bert/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="ner-bert",
-    version="0.0.1",
+    version="0.0.2",
     description="BERT-Language Model for NLP",
     packages=["script"],
     author="Jincheng Chen",
